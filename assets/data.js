e050dfa508d5b2704ad9e0e6c9239b6a37af919c
preethib20 <145477588+preethib20@users.noreply.github.com>
2024-12-09 17:00:21 -0500

    Create data.js

---
+++ assets/data.js
@@ -0,0 +1,6 @@
+const usedCars = [
+  { id: 1, year: 2012, make: "Toyota", mileage: 12000, price: 10000, color: "White", img: "https://via.placeholder.com/150" },
+  { id: 2, year: 2015, make: "Ford", mileage: 15000, price: 15000, color: "Black", img: "https://via.placeholder.com/150" },
+  { id: 3, year: 2018, make: "Lexus", mileage: 5000, price: 25000, color: "Silver", img: "https://via.placeholder.com/150" },
+  { id: 4, year: 2010, make: "Toyota", mileage: 20000, price: 8000, color: "Black", img: "https://via.placeholder.com/150" },
+];
