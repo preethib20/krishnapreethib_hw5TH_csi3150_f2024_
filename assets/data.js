c1f3aab3b9a56d2d3cff892afda64205ea58ec42
preethib20 <145477588+preethib20@users.noreply.github.com>
2024-12-09 17:30:08 -0500

    Update data.js

--- assets/data.js
+++ assets/data.js
@@ -1,6 +1,6 @@
 const usedCars = [
   { id: 1, year: 2012, make: "Toyota", mileage: 12000, price: 10000, color: "White", img: "https://images.app.goo.gl/Ebchq7Lpb6eXuoH89/150" },
-  { id: 2, year: 2015, make: "Ford", mileage: 15000, price: 15000, color: "Black", img: "https://via.placeholder.com/150" },
-  { id: 3, year: 2018, make: "Lexus", mileage: 5000, price: 25000, color: "Red", img: "https://via.placeholder.com/150" },
-  { id: 4, year: 2010, make: "Toyota", mileage: 20000, price: 8000, color: "Black", img: "https://via.placeholder.com/150" },
+  { id: 2, year: 2015, make: "Ford", mileage: 15000, price: 15000, color: "Black", img: "https://images.app.goo.gl/x6rL2dW8jd1RhC4b9/150" },
+  { id: 3, year: 2018, make: "Lexus", mileage: 5000, price: 25000, color: "Red", img: "https://images.app.goo.gl/oWzc2zv5XTB7KTaR6/150" },
+  { id: 4, year: 2010, make: "Toyota", mileage: 20000, price: 8000, color: "Black", img: "https://images.app.goo.gl/QMchbosScHccEiv77/150" },
 ];
